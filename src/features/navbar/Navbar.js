08eb175b3ab024fff8d88f25d1daa9c6077cc822
kamal-sethi <kamalsethi018@gmail.com>
2024-06-07 18:05:52 +0530

    extracted constants from navbar and created a helper js file

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -8,21 +8,7 @@ import {
 } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 
-const user = {
-  name: "Tom Cook",
-  email: "tom@example.com",
-  imageUrl:
-    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-};
-const navigation = [
-  { name: "Dashboard", href: "#", current: true },
-  { name: "Team", href: "#", current: false },
-];
-const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
-];
+import { userNavigation,navigation,user} from "./helper";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
